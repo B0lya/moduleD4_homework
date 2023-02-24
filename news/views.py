ba03cec154bd285bcc9bd1882fa5035ea8c18199
B0lya <114803631+B0lya@users.noreply.github.com>
2023-02-24 18:23:08 +0300

    Add files via upload

---
+++ news/views.py
@@ -0,0 +1,60 @@
+from django.shortcuts import render
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from .models import Post
+from django.core.paginator import Paginator
+from .filters import NewsFilter
+from .forms import PostForm
+
+
+class NewsList(ListView):
+    model = Post
+    template_name = 'news.html'
+    context_object_name = 'news'
+    queryset = Post.objects.order_by('-dateCreation')
+    paginate_by = 10
+
+class NewsDetail(DetailView):
+    model = Post
+    template_name = "post.html"
+    context_object_name = "post"
+
+
+class Search(ListView):
+    model = Post
+    template_name = 'search.html'
+    context_object_name = 'news'
+    queryset = Post.objects.order_by('-dateCreation')
+    paginate_by = 10
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['filter'] = NewsFilter(self.request.GET, queryset=self.get_queryset())
+        return context
+
+
+class NewsCreateView(CreateView):
+    template_name = 'news_create.html'
+    form_class = PostForm
+    success_url = '/news'
+
+
+class NewsUpdateView(UpdateView):
+    template_name = 'news_create.html'
+    form_class = PostForm
+    success_url = '/news'
+
+    
+    def get_object(self, **kwargs):
+        id = self.kwargs.get('pk')
+        return Post.objects.get(pk=id)
+
+
+class NewsDeleteView(DeleteView):
+    template_name = 'news_delete.html'
+    queryset = Post.objects.all()
+    success_url = '/news'
+
+    
+    def get_object(self, **kwargs):
+        id = self.kwargs.get('pk')
+        return Post.objects.get(pk=id)
